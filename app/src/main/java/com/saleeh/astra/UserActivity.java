2844cb684f92de233b555a053ffdb0b67e1e1de3
saleeh <saleeh@coffye.com>
2016-02-15 11:08:04 +0530

    Final

--- app/src/main/java/com/saleeh/astra/UserActivity.java
+++ app/src/main/java/com/saleeh/astra/UserActivity.java
@@ -3,22 +3,33 @@ package com.saleeh.astra;
 import android.content.Intent;
 import android.content.res.TypedArray;
 import android.databinding.DataBindingUtil;
+import android.graphics.Color;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
 import android.widget.Toast;
 
 import com.dd.processbutton.iml.ActionProcessButton;
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.parse.ParseException;
 import com.parse.ParseInstallation;
 import com.parse.SaveCallback;
+import com.saleeh.astra.api.ServiceAPI;
+import com.saleeh.astra.api.models.Group;
 import com.saleeh.astra.api.models.User;
 import com.saleeh.astra.databinding.ActivityUserBinding;
 
 import java.util.HashMap;
+import java.util.List;
 
-import bolts.Continuation;
-import bolts.Task;
+import dmax.dialog.SpotsDialog;
+import retrofit.Callback;
+import retrofit.Response;
+import retrofit.Retrofit;
 import uz.shift.colorpicker.OnColorChangedListener;
 
 public class UserActivity extends BaseActivity {
@@ -30,7 +41,15 @@ public class UserActivity extends BaseActivity {
     int currentColor;
     String groups[];
     private String currentDept = "CSE";
-    private HashMap<Integer, Integer> colors;
+    //  private HashMap<Integer, Integer> colors;
+
+    private HashMap<Integer, String> colorsMap = new HashMap<>();
+    int[] colors = new int[10];
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +57,37 @@ public class UserActivity extends BaseActivity {
         installation = ParseInstallation.getCurrentInstallation();
 
         binding = DataBindingUtil.setContentView(this, R.layout.activity_user);
+        final SpotsDialog dialog = new SpotsDialog(this);
+        dialog.show();
+        ServiceAPI.getInstance().getApiService().groups().enqueue(new Callback<List<Group>>() {
+            @Override
+            public void onResponse(Response<List<Group>> response, Retrofit retrofit) {
+                int i = 0;
+                for (Group group : response.body()) {
+                    colors[i] = Color.parseColor(group.color);
+                    colorsMap.put(colors[i], group.name);
+                    i++;
+
+                }
+                binding.picker.setColors(colors);
+                binding.picker.setSelectedColorPosition(0);
+                //     binding.picker.setLayoutParams(new );
+                dialog.hide();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                Toast.makeText(UserActivity.this, t.getMessage(), Toast.LENGTH_LONG).show();
+
+            }
+        });
         TypedArray ta = getResources().obtainTypedArray(R.array.colors);
         groups = getResources().getStringArray(R.array.groups);
-        colors = new HashMap<>();
-        for (int i = 0; i < ta.length(); i++) {
-            colors.put(ta.getColor(i, 0), i);
-        }
-        ta.recycle();
+        //      colors = new HashMap<>();
+        //    for (int i = 0; i < ta.length(); i++) {
+        //      colors.put(ta.getColor(i, 0), i);
+        //  }
+        //ta.recycle();
         binding.spinner.setItems(sems);
         binding.picker.setOnColorChangedListener(new OnColorChangedListener() {
             @Override
@@ -91,11 +134,53 @@ public class UserActivity extends BaseActivity {
             finish();
         }
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
     public String getDept(int color) {
-        return groups[colors.get(color)];
-
+        //    return groups[colors.get(color)];
+        return colorsMap.get(color);
     }
 
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "User Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.saleeh.astra/http/host/path")
+        );
+        AppIndex.AppIndexApi.start(client, viewAction);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        Action viewAction = Action.newAction(
+                Action.TYPE_VIEW, // TODO: choose an action type.
+                "User Page", // TODO: Define a title for the content shown.
+                // TODO: If you have web page content that matches this app activity's content,
+                // make sure this auto-generated web page URL is correct.
+                // Otherwise, set the URL to null.
+                Uri.parse("http://host/path"),
+                // TODO: Make sure this auto-generated app URL is correct.
+                Uri.parse("android-app://com.saleeh.astra/http/host/path")
+        );
+        AppIndex.AppIndexApi.end(client, viewAction);
+        client.disconnect();
+    }
 }
